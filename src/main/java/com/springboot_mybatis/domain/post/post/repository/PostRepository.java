9cfb813afe86ce68cb896a791571e35130db4206
kjh3165 <jhwk109@gmail.com>
2026-05-14 15:01:13 +0900

    Mapper xml 파일 분리

--- src/main/java/com/springboot_mybatis/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/springboot_mybatis/domain/post/post/repository/PostRepository.java
@@ -8,135 +8,29 @@ import java.util.List;
 @Mapper
 public interface PostRepository {
 
-    @Select("""
-    <script>
-    SELECT * 
-    FROM post
-    </script>
-    """)
     List<Post> findAll();
 
-    @Select("""
-    <script>
-    SELECT *
-    FROM post
-    WHERE id = #{id}
-    </script>
-    """)
     Post findById(int id);
 
-    @Insert("""
-    <script>
-    INSERT INTO post
-    SET createDate = NOW(),
-        modifyDate = NOW(),
-        title = #{title},
-        content = #{content}
-    </script>
-    """)
-    @Options(useGeneratedKeys = true, keyProperty = "id")
     int create(Post post);
 
-    @Insert("""
-    <script>
-    INSERT INTO post
-    SET createDate = NOW(),
-        modifyDate = NOW(),
-        title = #{title},
-        content = #{content}
-    </script>
-    """)
-    void createV2(String title, String content);
+    void createV2(
+            @Param("title") String title,
+            @Param("content") String content
+    );
 
-    @Select("""
-    SELECT LAST_INSERT_ID()
-    """)
     int getLastInsertId();
 
-    @Delete("""
-    DELETE
-    FROM post
-    WHERE id = #{id}
-    """)
     void deleteById(int id);
 
-    @Update("""
-    <script>
-    UPDATE post
-    <set> 
-        modifyDate = NOW(),
-        <if test="title != null and title != ''">
-            title = #{title},
-        </if>
-        <if test="content != null and content != ''">
-            content = #{content},
-        </if>
-    </set>
-    <where>
-        <if test="id != null and id > 0">
-            id = #{id}
-        </if>
-    </where>
-    </script>
-    """)
     int update(@Param("id") int id,
                @Param("title") String title,
                @Param("content") String content);
 
-    @Select("""
-    <script>
-    SELECT *
-    FROM post
-    <where>
-        <choose>
-            <when test="kwType == 'title'">
-                title LIKE CONCAT('%', #{kw}, '%')
-            </when>
-            <when test="kwType == 'content'">
-                content LIKE CONCAT('%', #{kw}, '%')
-            </when>
-            <otherwise>
-                (
-                    title LIKE CONCAT('%', #{kw}, '%')
-                    OR
-                    content LIKE CONCAT('%', #{kw}, '%')
-                )
-            </otherwise>
-        </choose>
-    </where>
-    </script>
-    """)
     List<Post> search(@Param("kwType") String kwType,
                       @Param("kw") String kw);
 
-    @Select("""
-    <script>
-    SELECT *
-    FROM post
-    <if test="orderBy != null and orderBy != ''">
-        ORDER BY
-        <choose>
-            <when test="orderBy == 'title'">title</when>
-            <when test="orderBy == 'createDate'">createDate</when>
-            <when test="orderBy == 'modifyDate'">modifyDate</when>
-        </choose>
-    </if>
-    <if test="orderOption != null and orderOption.toLowerCase() == 'desc'">
-        DESC
-    </if>
-    </script>
-    """)
     List<Post> findAllOrdered(String orderBy, String orderOption);
 
-    @Delete("""
-    <script>
-    DELETE
-    FROM post
-    WHERE id IN 
-    <foreach collection="ids" item="id" open="(" separator="," close=")">
-        #{id}
-    </foreach>
-    </script>
-    """)
     int deleteByIds(List<Integer> ids);
 }
