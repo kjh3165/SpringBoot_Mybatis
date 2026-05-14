24b938cecb002a9e7b4870d567b57108f24e2151
kjh3165 <jhwk109@gmail.com>
2026-05-14 15:20:06 +0900

    회원 다건 조회 테스트 케이스

--- src/main/java/com/springboot_mybatis/domain/member/member/repository/MemberRepository.java
+++ src/main/java/com/springboot_mybatis/domain/member/member/repository/MemberRepository.java
@@ -1,7 +1,11 @@
 package com.springboot_mybatis.domain.member.member.repository;
 
+import com.springboot_mybatis.domain.member.member.dto.Member;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface MemberRepository {
+    List<Member> findAll();
 }
