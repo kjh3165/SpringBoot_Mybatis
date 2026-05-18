c1579705723d29a41bd78c562eff1808b63c7328
kjh3165 <jhwk109@gmail.com>
2026-05-18 09:30:44 +0900

    인터셉터 개요 및 등록 후 로그 확인

---
+++ src/main/java/com/springboot_mybatis/global/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.springboot_mybatis.global.config;
+
+import com.springboot_mybatis.global.interceptor.BeforeActionInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final BeforeActionInterceptor beforeActionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration registration =  registry.addInterceptor(beforeActionInterceptor);
+
+        registration.addPathPatterns("/**");
+    }
+}
