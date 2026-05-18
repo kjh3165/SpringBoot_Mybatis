93a7ee63e344c857ab7c19b421d3f7387b677c8c
kjh3165 <jhwk109@gmail.com>
2026-05-18 10:12:07 +0900

    PostController, 게시글 작성, 목록, 상세 페이지 추가

---
+++ src/main/java/com/springboot_mybatis/domain/post/post/controller/PostController.java
@@ -0,0 +1,33 @@
+package com.springboot_mybatis.domain.post.post.controller;
+
+import com.springboot_mybatis.domain.post.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/posts")
+@RequiredArgsConstructor
+@Controller
+public class PostController {
+    private final PostService postService;
+
+    @GetMapping("/write")
+    public String write() {
+
+        return "post/post/write";
+    }
+
+    @GetMapping("/list")
+    public String list() {
+        return "post/post/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(@PathVariable Integer id, Model model) {
+        model.addAttribute("id", id);
+        return "post/post/detail";
+    }
+}
